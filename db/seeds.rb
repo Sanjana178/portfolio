389178fc06dd372d24054efd299f2abed5c7558b
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-12-25 17:25:58 +0100

    Added contents model with migration and created the front end with questionare page as well

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating contents...'
+Content.create!({
+  title: "Headline",
+  description: "Tina Damon"
+})
+Content.create!({
+  title: "About",
+  address: "<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dignissimos, omnis esse. Quos porro iure ipsam facere cum at! Qui, sit. Deserunt nihil, animi voluptate accusamus sunt distinctio magni labore.</div><div>Odit voluptatum autem, libero omnis facilis architecto voluptatibus, blanditiis accusantium placeat ad vel rem numquam quae non aspernatur modi illo inventore rerum. Architecto dolore, et nostrum repudiandae quidem corporis soluta.</div>"
+})
+puts 'Finished!'
